5f73e8dd4a0beddfaae5b46e2ea4f338825516c5
Erden <erden.aidynuly>
2024-09-07 16:29:43 +0500

    fix

--- apps/content/views.py
+++ apps/content/views.py
@@ -58,19 +58,15 @@ class MyTopicView(PrivateSONRendererMixin, ListCreateAPIView, DestroyAPIView):
         return Response({})
 
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        my_course_ids = queryset.values_list('topic_id', flat=True)
-        new_topics = Topic.objects.filter(
-            Q(chapter__course__subject__direction__enable_all=True) & ~Q(id__in=my_course_ids)
-        ).all()
-        grouped_topics = groupby(sorted(new_topics, key=lambda t: t.chapter.course_id),
-                                 key=lambda t: t.chapter.course_id)
-        first_topics = [next(topics) for course_id, topics in grouped_topics]
-        for topic in first_topics:
-            MyTopic.objects.create(user=request.user, topic=topic,
-                                   course=topic.chapter.course)
-
         queryset = self.get_queryset().filter(user=self.request.user)
+        my_course_ids = queryset.values_list('course_id', flat=True)
+        new_courses = Course.objects.filter(
+            Q(subject__direction__enable_all=True) & ~Q(id__in=my_course_ids)
+        ).all()
+        for course in new_courses:
+            if course.chapters.exists() and course.chapters.first().topics.exists():
+                MyTopic.objects.create(user=request.user, topic=course.chapters.first().topics.first(),
+                                       course=course)
         active = request.query_params.get('active', 'false').lower() == 'true'
         if active:
             queryset = queryset.filter(is_completed=False)
