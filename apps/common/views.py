ec51791ee83a8413e64c3471b96da1c1361cb357
Erden <erden.aidynuly>
2024-10-11 15:02:28 +0500

    addedn analytics

--- apps/common/views.py
+++ apps/common/views.py
@@ -5,11 +5,13 @@ from django.conf import settings
 from django.utils import timezone
 
 from apps import Roles
+from apps.analytics.models import Quiz
 from apps.common.mixins import PublicJSONRendererMixin
 from rest_framework.response import Response
 
-from apps.users.models import User
-
+from apps.content.models import School
+from apps.location.models import City
+from apps.users.models import User, UserQuizReport, UserQuizQuestion
 
 MONTHS_NAMES = {
     "1": "Январь",
@@ -54,6 +56,7 @@ def dashboard_view(request):
     users_total = User.objects.filter(created_at__gte=filter_period)
     users_count_total = users_total.count()
     students_count_total = users_total.filter(role=Roles.STUDENT).count()
+    cities = City.objects.all()
 
     if period == 'last_year':
         users_dates = users_total.annotate(month_year=MonthYear('created_at')).values(group_by_field).annotate(
@@ -64,13 +67,83 @@ def dashboard_view(request):
     users_dates_list = []
     users_count_list = []
     for item in users_dates:
-        users_dates_list.append(get_month_str(item[group_by_field]) if period=="last_year" else item[group_by_field])
+        users_dates_list.append(get_month_str(item[group_by_field]) if period == "last_year" else item[group_by_field])
         users_count_list.append(item['users_count'])
 
     return render(request, "dashboard.html", {
         "period": period,
+        "cities": cities,
         "users_count_total": users_count_total,
         "students_count_total": students_count_total,
         "users_dates_list": users_dates_list,
         "users_count_list": users_count_list
     })
+
+
+from django.http import JsonResponse
+
+
+def get_schools(request):
+    region_id = request.GET.get('region_id')
+    schools = School.objects.filter(city_id=region_id).values('id', 'name')
+    return JsonResponse(list(schools), safe=False)
+
+
+def get_students(request):
+    school_id = request.GET.get('school_id')
+    students = User.objects.filter(school_id=school_id).values('id', 'full_name', 'mobile_phone')
+    return JsonResponse(list(students), safe=False)
+
+
+def get_tasks_data(request):
+    period = request.GET.get('task_period')  # today/last_week/last_month/last_year
+    group_by_field = 'created_at__date'
+    if period == 'today':
+        filter_period = timezone.now().date()
+    elif period == 'last_week':
+        filter_period = timezone.now() - timezone.timedelta(days=7)
+    elif period == 'last_year':
+        group_by_field = 'month_year'
+        filter_period = timezone.now() - timezone.timedelta(days=365)
+    else:
+        period = 'last_month'
+        filter_period = timezone.now() - timezone.timedelta(days=30)
+
+    region_id = request.GET.get('region_id')
+    school_id = request.GET.get('school_id')
+    student_id = request.GET.get('student_id')
+
+    user_questions = UserQuizQuestion.objects.all()
+
+    if region_id and region_id != 'all':
+        user_questions = user_questions.filter(user__school__city_id=region_id)
+
+    if school_id and school_id != 'all':
+        user_questions = user_questions.filter(user__school_id=school_id)
+
+    if student_id and student_id != 'all':
+        user_questions = user_questions.filter(user_id=student_id)
+
+    users_dates_list = []
+    user_tasks_list = []
+
+    user_resolved_tasks = user_questions.annotate(month_year=MonthYear('created_at')).values(group_by_field).annotate(
+        resolved_task_count=Count('id')).order_by(
+        group_by_field)
+
+    for item in user_resolved_tasks:
+        users_dates_list.append(item[group_by_field])
+        user_tasks_list.append(item['resolved_task_count'])
+
+    tasks_solved = user_questions.filter(created_at__gte=filter_period).count()
+    tasks_correct = user_questions.filter(is_correct=True, created_at__gte=filter_period).count()
+
+    data = {
+        'users_dates_list': users_dates_list,
+        'user_tasks_list': user_tasks_list,
+        'tasks_solved': tasks_solved,
+        'tasks_correct': tasks_correct,
+        'percent_correct': (tasks_correct / tasks_solved) * 100 if tasks_solved > 0 else 0
+    }
+
+    return JsonResponse(data)
--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
-from apps.common.views import dashboard_view
+from apps.common.views import dashboard_view, get_schools, get_students, get_tasks_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('ckeditor/', include('ckeditor_uploader.urls')),
     path('dashboard/', dashboard_view),
+    path('get-schools/', get_schools, name='get_schools'),
+    path('get-students/', get_students, name='get_students'),
+    path('get-tasks-data/', get_tasks_data, name='get_tasks_data'),
     path("api/", include("apps.urls")),
 ]
 
