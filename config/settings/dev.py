409f87b5c981a8550b87f7bb47e34c8f246c60a3
Erden <erden.aidynuly>
2024-10-11 21:40:06 +0500

    added analytics

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -4,7 +4,7 @@ DEBUG = True
 SITE_DOMAIN = "http://127.0.0.1:8008"
 FCM_CREDENTIALS_PATH = "/credentials/fcm-prod.json"
 
-
-DATABASES['default']['NAME'] = "onaidb"
-DATABASES['default']['HOST'] = '195.49.215.16'
-DATABASES['default']['PORT'] = '5432'
+#
+# DATABASES['default']['NAME'] = "onaidb"
+# DATABASES['default']['HOST'] = '195.49.215.16'
+# DATABASES['default']['PORT'] = '5432'
