4571adc0eb7bff5d004dc7c9fa79612a9216a937
Erden <erden.aidynuly>
2024-10-28 21:11:02 +0500

    fix latex bracket

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,18 +3,6 @@ from .base import *
 FCM_CREDENTIALS_PATH = "/credentials/fcm-prod.json"
 
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
-REDIS_AS_CACHE_URL = "redis://:{password}@{host}:{port}/{db_index}".format(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    password=REDIS_PASSWORD,
-    db_index=REDIS_DB_FOR_CACHE,
-)
-REDIS_AS_BROKER_URL = "redis://:{password}@{host}:{port}/{db_index}".format(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    password=REDIS_PASSWORD,
-    db_index=REDIS_DB_FOR_CELERY,
-)
 CONSTANCE_REDIS_CONNECTION = REDIS_AS_CACHE_URL
 CACHES['default']['LOCATION'] = REDIS_AS_CACHE_URL
 CELERY_BROKER_URL = REDIS_AS_BROKER_URL
